2aeb9edd4c1bba75609eef8fcf82a738ef2d9250
ahcordesign <ahcordesign@gmail.com>
2011-12-09 16:04:30 +0000

    Atualizados: Classe DocFactory : ativado o suporte as neeosTags "_style" e "_script"
    em 09/12/2011

---
+++ core/neos/helper/neostag/select.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright	NEOS PHP Framework - http://neosphp.org
+ * @license		http://neosphp.org/license Todos os direitos reservados - proibida a utilização deste material sem prévia autorização.
+ * @author		Paulo R. B. Rocha - prbr@ymail.com
+ * @package		Neos\Helper
+ * @subpackage	NeosTags
+ */ 
+  if(!function_exists('_neostag_select')){function _neostag_select($ret){global $vartemp,$_neos_vars,$nomeView;if(isset($ret['var'])){if(isset($_neos_vars[0][trim($ret['var'])])){$v=$_neos_vars[0][trim($ret['var'])];}else{$v='';}if(is_string($nomeView)&&$_neos_vars[$nomeView][trim($ret['var'])]!=''){$v=$_neos_vars[$nomeView][trim($ret['var'])];}unset($ret['var'],$ret['-inicio-'],$ret['-tamanho-'],$ret['-final-'],$ret['-tipo-'],$ret['conteudo']);if($v!=''){$ul='';foreach($ret as $key=>$value){if($ul==''){$ul='<select';}if(trim($key)=='multiple'){$ul.=' '.trim($key);}else{$ul.=' '.trim($key).'="'.trim($value).'"';}unset($ret[$key]);}if($ul!=''){$vartemp=$ul.=">\n";}else{$vartemp="<select>\n";}foreach($v as $k=>$vl){$vartemp.='<option value="'.$k.'" ';if(is_array($vl)){$vartemp.=' selected="selected" >'.$vl[0]."</option>\n";}else{$vartemp.='>'.$vl."</option>\n";}}$vartemp.="</select>\n";}}}}
---
+++ core/neos/helper/neostag/var.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright	NEOS PHP Framework - http://neosphp.org
+ * @license		http://neosphp.org/license Todos os direitos reservados - proibida a utilização deste material sem prévia autorização.
+ * @author		Paulo R. B. Rocha - prbr@ymail.com
+ * @package		Neos\Helper
+ * @subpackage	NeosTags
+ */ 
+  if(!function_exists('_neostag_var')){
+	
+	function _neostag_var($ret){
+		global $_neos_vars,$nomeView;
+		$vartemp='';
+		if(isset($_neos_vars[0][trim($ret['var'])])){$v=$_neos_vars[0][trim($ret['var'])];}else{$v='';}
+		
+		if(is_string($nomeView)&&$_neos_vars[$nomeView][trim($ret['var'])]!=''){$v=$_neos_vars[$nomeView][trim($ret['var'])];}
+		
+		unset($ret['var'],$ret['-inicio-'],$ret['-tamanho-'],$ret['-final-'],$ret['-tipo-'],$ret['conteudo']);
+		
+		if($v!=''){
+			$d='';
+			foreach($ret as $key=>$value){
+				if($d==''){$d='<div';}
+				$d.=' '.trim($key).'="'.trim($value).'"';
+				unset($ret[$key]);
+			}
+			if($d!=''){$vartemp=$d.='>'.$v.'</div>';}else{$vartemp=$v;}
+		}
+	return $vartemp;
+	}
+}
