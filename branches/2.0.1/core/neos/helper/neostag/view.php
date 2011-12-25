b6785960e5240b054b32c896d61a1b3fdc01083e
ahcordesign@gmail.com <ahcordesign@gmail.com@ce9736f9-0142-a106-f82d-23e23c4e2e34>
2011-12-25 16:54:23 +0000

    Correção do sistema de Erros e autoLoader (classes) - 25/12/2011

---
+++ branches/2.0.1/core/neos/helper/neostag/view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @copyright	NEOS PHP Framework - http://neosphp.org
+ * @license		http://neosphp.org/license Todos os direitos reservados - proibida a utilização deste material sem prévia autorização.
+ * @author		Paulo R. B. Rocha - prbr@ymail.com
+ * @package		Neos\Helper
+ * @subpackage	NeosTags
+ */ 
+  if(!function_exists('_neostag_view')){function _neostag_view($ret){if(isset($ret['name'])){if(file_exists(APP_VIEW .$ret['name'].'.html')){return file_get_contents(APP_VIEW .$ret['name'].'.html');}}}}
