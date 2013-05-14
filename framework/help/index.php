3f284b93823fb25a3ac58c70ac0ecedac006e7f9
ahcordesign <ahcordesign@gmail.com>
2013-05-14 15:56:18 +0000

    Nova versão de trabalho ocupando o Trunk

--- framework/help/index.php
+++
@@ -1,7 +0,0 @@
-<?php include PPATH . 'inc/header.php';?>
-
-		<?php echo APP_ERROR; echo 'RPATH: ' . RPATH . '<br>PPATH: ' . PPATH . '<br>URL_BASEX: ' . URL_BASEX; /*_pt($_SERVER);*/?>
-            
-            <p class="ops"></p>
-            
-<?php include PPATH . 'inc/footer.php';?>
